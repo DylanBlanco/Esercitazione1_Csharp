7f33dcb51157d5d29c45ae6b87ed2a2ab26ef2f9
DylanBlanco <dylanblanco1198@gmail.com>
2025-10-22 18:16:07 +0200

    snack3 - CREA UN ARRAY DI 10 NUMERI E STAMPA SOLO I NUMERI DISPARI

--- Esercitazione1_Csharp/Program.cs
+++ Esercitazione1_Csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Esercitazione1_Csharp
 {
@@ -10,7 +11,7 @@ namespace Esercitazione1_Csharp
         //    Console.WriteLine("Hello World!!!");
         //}
 
-        // CHIEDI A UTENTE 2 NUMERI E STAMPA LA SOMMA
+        // snack1 - CHIEDI A UTENTE 2 NUMERI E STAMPA LA SOMMA
         //static void Main(string[] args)
         //{
         //    Console.WriteLine("Inserisci il primo numero: ");
@@ -40,37 +41,65 @@ namespace Esercitazione1_Csharp
         //    }
         //}
 
-        // CALCOLA AREA E PERIMETRO DEL RETTANGOLO
+        //  snack2 - CALCOLA AREA E PERIMETRO DEL RETTANGOLO
+        //static void Main(string[] arg)
+        //{
+        //    Console.WriteLine("Inserisci la lunghezza del rettangolo: ");
+        //    int lunghezza = int.Parse(Console.ReadLine());
+
+        //    Console.WriteLine("Inserisci la larghezza del rettangolo: ");
+        //    int larghezza = int.Parse(Console.ReadLine());
+
+        //    int area = lunghezza * larghezza;
+        //    int perimetro = 2 * (lunghezza + larghezza);
+        //    Console.WriteLine("L'area del rettangolo è: " + area);
+        //    Console.WriteLine("Il perimetro del rettangolo è: " + perimetro);
+
+        //    // VERSIONE CON GESTIONE ERRORI
+        //    Console.Write("Inserisci la lunghezza del rettangolo: ");
+        //    bool isLengthValid = double.TryParse(Console.ReadLine(), out double length);
+
+        //    Console.Write("Inserisci la larghezza del rettangolo: ");
+        //    bool isWidthValid = double.TryParse(Console.ReadLine(), out double width);
+
+        //    if (isLengthValid && isWidthValid)
+        //    {
+        //        double area2 = length * width;
+        //        double perimeter = 2 * (length + width);
+        //        Console.WriteLine($"L'area del rettangolo è: {area2}");
+        //        Console.WriteLine($"Il perimetro del rettangolo è: {perimeter}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Errore: uno dei valori inseriti non è un numero valido.");
+        //    }
+        //}
+
+        // snack3 - CREA UN ARRAY DI 10 NUMERI E STAMPA SOLO I NUMERI DISPARI
         static void Main(string[] arg)
         {
-            Console.WriteLine("Inserisci la lunghezza del rettangolo: ");
-            int lunghezza = int.Parse(Console.ReadLine());
+            int numeroElementi = 5;
+            int[] arrayNum = new int[numeroElementi];
 
-            Console.WriteLine("Inserisci la larghezza del rettangolo: ");
-            int larghezza = int.Parse(Console.ReadLine());
-
-            int area = lunghezza * larghezza;
-            int perimetro = 2 * (lunghezza + larghezza);
-            Console.WriteLine("L'area del rettangolo è: " + area);
-            Console.WriteLine("Il perimetro del rettangolo è: " + perimetro);
-
-            // VERSIONE CON GESTIONE ERRORI
-            Console.Write("Inserisci la lunghezza del rettangolo: ");
-            bool isLengthValid = double.TryParse(Console.ReadLine(), out double length);
-
-            Console.Write("Inserisci la larghezza del rettangolo: ");
-            bool isWidthValid = double.TryParse(Console.ReadLine(), out double width);
-
-            if (isLengthValid && isWidthValid)
+            Random generatore = new Random();  // generatore di num casuale
+            for (int i=0; i<numeroElementi; i++)
             {
-                double area2 = length * width;
-                double perimeter = 2 * (length + width);
-                Console.WriteLine($"L'area del rettangolo è: {area2}");
-                Console.WriteLine($"Il perimetro del rettangolo è: {perimeter}");
+                int numero = generatore.Next(1, 11); // genera un numero casuale tra 1 e 10
+                arrayNum[i] = numero;  //salva num in array
             }
-            else
+            // visualizza array generato
+            foreach (int numero in arrayNum)
             {
-                Console.WriteLine("Errore: uno dei valori inseriti non è un numero valido.");
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("Numeri dispari nell'array:");
+            foreach (int numero in arrayNum)
+            {
+                if (numero % 2 != 0) // verifica se il numero è dispari
+                {
+                    Console.WriteLine(numero); // stampa il numero dispari
+                }
             }
         }
     }
